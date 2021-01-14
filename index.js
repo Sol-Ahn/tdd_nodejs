0523a9b7dbb2d2920819c621cb2b577ca8d1c103
Sol-Ahn <sorry0194@gmail.com>
2021-01-15 00:27:14 +0900

    Modify: index.js

--- index.js
+++ index.js
@@ -1,12 +1,19 @@
 // Hello World example
 const express = require("express");
+const morgan = require("morgan");
 const app = express();
-const port = 3000;
+const users = [
+  { id: 1, name: "alice" },
+  { id: 2, name: "tom" },
+  { id: 3, name: "chris" },
+];
 
-app.get("/", (req, res) => {
-  res.send("Hello World!");
+app.use(morgan("dev"));
+
+app.get("/users", (req, res) => {
+  res.send(users);
 });
 
-app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`);
+app.listen(3000, () => {
+  console.log(`Example app listening at http://localhost:3000`);
 });
